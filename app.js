e0b394f9f2f218630c6a881d66773eb9d0166949
Magne Alvheim <magne@alvheim.com>
2020-08-24 15:20:39 +0200

    Board opprettet med layout (8:17)

--- app.js
+++ app.js
@@ -34,4 +34,35 @@ document.addEventListener('DOMContentLoaded', () => {
         1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
     ]
+
+    const squares = []
+
+    // Legend
+    // 0 - Pac dot
+    // 1 = wall
+    // 2 = ghost-lair
+    // 3 = power pellet
+    // 4 = empty
+
+    // Draw the grid and render it
+    function createBoard() {
+        for (let i = 0; i < layout.length; i++) {
+            const square = document.createElement('div')
+            grid.appendChild(square)
+            squares.push(square)
+            
+            // Add laout to the board
+            if (layout[i] === 0) {
+                squares[i].classList.add('pack-dot')
+            } else if (layout[i] === 1) {
+                squares[i].classList.add('wall')
+            } else if (layout[i] === 3) {
+                squares[i].classList.add('power-pellet')
+            }
+
+        }
+    }
+
+    createBoard()
+
 })
