27f67efed78ecedba0b2c661189f4348c0c9d5b0
Magne Alvheim <magne@alvheim.com>
2020-08-27 14:05:21 +0200

    Ghosts added to the board (21:56)

--- app.js
+++ app.js
@@ -131,4 +131,27 @@ document.addEventListener('DOMContentLoaded', () => {
             squares[pacmanCurrentIndex].classList.remove('pac-dot')
         }
     }
+
+    class Ghost {
+        constructor(className, startIndex, speed) {
+            this.className = className
+            this.startIndex = startIndex
+            this.speed = speed
+            this.currentIndex = startIndex
+            this.timerId = NaN
+        }
+    }
+
+    ghosts = [
+        new Ghost('blinky', 348, 250),
+        new Ghost('pinky', 376, 400),
+        new Ghost('inky', 351, 300),
+        new Ghost('clyde', 379, 500)
+    ]
+
+    // draw my ghosts to onto the grid
+    ghosts.forEach(ghost => {
+        squares[ghost.currentIndex].classList.add(ghost.className)
+        squares[ghost.currentIndex].classList.add('ghost')
+    });
 })
